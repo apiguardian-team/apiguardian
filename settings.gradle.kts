f05138ba668994746ee1323f517ff4db1965d599
Marc Philipp <mail@marcphilipp.de>
2024-09-16 11:02:04 +0200

    Force Gradle to use JDK 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "apiguardian-api"
-
-require(JavaVersion.current().isJava11) {
-    "The @API Guardian build requires Java 11. Currently executing with Java ${JavaVersion.current()}."
-}
