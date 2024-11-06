999f353805865f85f283a8743cd0788483034c06
Marc Philipp <mail@marcphilipp.de>
2024-11-06 10:15:39 +0100

    Set up Build Scan publication

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,26 @@
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
+}
+
 rootProject.name = "apiguardian-api"
+
+develocity {
+    buildScan {
+        val isCiServer = System.getenv("CI") != null
+
+        server = "https://ge.junit.org"
+        uploadInBackground = !isCiServer
+
+        obfuscation {
+            if (isCiServer) {
+                username { "github" }
+            } else {
+                hostname { null }
+                ipAddresses { emptyList() }
+            }
+        }
+
+        publishing.onlyIf { it.isAuthenticated }
+    }
+}
